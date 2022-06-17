2d3c1a17b1198cac322a30101d7dc23fe8f04402
MilicTG <milic.tg@gmail.com>
2022-06-17 20:40:37 +0200

    Row and Column

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/widgets/card/intro_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,24 +8,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(25.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(.15),
-                offset: const Offset(0, 15),
-                blurRadius: 25,
-              ),
-            ],
-            borderRadius: BorderRadius.circular(15.0),
-            border: Border.all(
-              color: Colors.black,
-              width: 1,
-            ),
-          ),
-          child: const Text("Welcome! My name is Ivan"),
+        child: IntroCard(
+          text: "Hay, I'm Ivan",
         ),
       ),
     );
